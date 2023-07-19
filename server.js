22e45950174ceac4499eda0279e49d09959ce6d3
Karan Raj <karanrajgodansa2003@gmail.com>
2023-07-19 22:30:24 +0530

    add .env

--- server.js
+++ server.js
@@ -5,22 +5,23 @@ const path = require('path');
 const axios = require('axios');
 const keyFilePath = path.join('C:\\Users\\karan\\OneDrive\\Desktop\\NewsAPIapp', 'localhost-key.pem');
 const certFilePath = path.join('C:\\Users\\karan\\OneDrive\\Desktop\\NewsAPIapp', 'localhost.pem');
+require('dotenv').config();
 
 const cors = require('cors');
 const app = express();
-
+const apiKey = process.env.API_KEY;
 const port = 3000;
 app.use(cors({
-    origin: (origin, callback) => {
-      // Check if the request comes from the allowed origin
-      const allowedOrigins = ['http://127.0.0.1:5500', 'https://karan2198.github.io'];
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-  }));
+  origin: (origin, callback) => {
+    // Check if the request comes from the allowed origin
+    const allowedOrigins = ['http://127.0.0.1:5500', 'https://karan2198.github.io'];
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+}));
 
 app.get('/', (req, res) => {
   res.redirect('/news');
@@ -29,7 +30,6 @@ app.get('/', (req, res) => {
 app.get('/news', async (req, res) => {
   const page = req.query.page || 1;
   const q = req.query.q || 'sports';
-  const apiKey = '893e90406e96486da7efc4244c066da2';
   const url = `https://newsapi.org/v2/everything?q=${q}&from=2023-07-15&to=2023-07-15&pageSize=20&page=${page}&sortBy=popularity&apiKey=${apiKey}`;
   try {
     const newsResponse = await axios.get(url);
